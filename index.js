86033b9fba4740ddba81641895babcb0528d4659
Senal Perera <senalperera777@gmail.com>
2026-01-23 12:23:36 +0530

    Initiated files
    - added some comments for structure

--- index.js
+++ index.js
@@ -2,4 +2,19 @@
 1 meter = 3.281 feet
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
+
+Follow the design spec
+
+Generate all conversions when
+the user clicks "Convert"
+
+Round the numbers down to
+three decimal places
+*/
+
+
+/*
+<div class="card">
+    <p id="unit-header"></p>
+</div>
 */
