030263a53bc96aa1e948e9de460e0d1083319c3c
Senal Perera <senalperera777@gmail.com>
2026-02-21 02:26:14 +0530

    Component + logic updates
    - Added JS logic for unit conversion
    - Added drop down component to represent units

--- Unit Converter/src/App.jsx
+++ Unit Converter/src/App.jsx
@@ -1,35 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import DropDown from './DropDown.jsx'
 import './App.css'
 
 function App() {
   const [inputValue, setInputValue] = useState("");
+  // Converting each value into metres as a base unit
+  const toMetres = {
+    metre: 1,
+    kilometre: 1000,
+    centimetre: 0.01,
+    millimetre: 0.001,
+    micrometre: 0.000001,
+    nanometre: 0.000000001,
+    inch: 0.0254,
+    foot: 0.3048,
+    mile: 1609.344,
+    yard: 0.9144,
+  }
 
+  // Coverting metres to rest of the values
+  const fromMetres = {
+    metre: 1,
+    kilometre: 0.001,
+    centimetre: 100,
+    millimetre: 1000,
+    micrometre: 1000000,
+    nanometre: 1000000000,
+    inch: 39.3701,
+    foot: 3.28084,
+    mile: 0.000621371,
+    yard: 1.09361,
+  };
+  
   const convertLogic = () => {
-    // convert logic
-
-
-    /*
-    1 meter = 3.281 feet
-    1 liter = 0.264 gallon
-    1 kilogram = 2.204 pound
-
-    Follow the design spec
-
-    Generate all conversions when
-    the user clicks "Convert"
-
-    Round the numbers down to
-    three decimal places
-    */
-
-
-    /*
-    <div class="card">
-        <p id="unit-header"></p>
-    </div>
-    */
+    const num = parseFloat(inputValue)
+    const inMetres = num * toMetres[unit]
+    const result = inMetres * fromMetres[targetUnit]
+    console.log(result.toFixed(3))
   }
 
   return (
@@ -50,19 +57,9 @@ function App() {
               }
             }}
           />
+          <DropDown />
           <p>to</p>
-          <select id="unit-select" className="input-bar">
-            <option value="metre">Metre</option>
-            <option value="kilometre">Kilometre</option>
-            <option value="centimetre">Centimetre</option>
-            <option value="millimetre">Millimetre</option>
-            <option value="micrometre">Micrometre</option>
-            <option value="nanometre">Nanometre</option>
-            <option value="inch">Inch</option>
-            <option value="foot">Foot</option>
-            <option value="mile">Mile</option>
-            <option value="yard">Yard</option>
-          </select>
+          <DropDown />
           <button id="convert-btn" onClick={convertLogic}>Convert</button>
         </div>
       </div>
@@ -71,6 +68,5 @@ function App() {
     </>
   )
 }
-
 
 export default App
---
+++ Unit Converter/src/DropDown.jsx
@@ -0,0 +1,21 @@
+function DropDown(){
+    return (
+        <>
+            <select id="unit-select" className="input-bar">
+                <option value="metre">Metre</option>
+                <option value="kilometre">Kilometre</option>
+                <option value="centimetre">Centimetre</option>
+                <option value="millimetre">Millimetre</option>
+                <option value="micrometre">Micrometre</option>
+                <option value="nanometre">Nanometre</option>
+                <option value="inch">Inch</option>
+                <option value="foot">Foot</option>
+                <option value="mile">Mile</option>
+                <option value="yard">Yard</option>
+            </select>
+        </>
+    )
+    
+}
+
+export default DropDown
